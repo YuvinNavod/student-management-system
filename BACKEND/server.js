b82fa253be288c70dc145ce1167acb09e4bb7850
Yuvin Navod <137191227+YuvinNavod@users.noreply.github.com>
2024-06-04 15:50:42 +0530

    update

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
 
+require("dotenv").config();
+
 const PORT = process.env.PORT || 8070;
 
 app.use(cors());
@@ -13,10 +15,10 @@ app.use(bodyParser.json());
 const URL = process.env.MONGODB_URL;
 
 mongoose.connect(URL, {
-    useCreateIndex: true,
+    
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+   
 });
 
 const connection = mongoose.connection;
