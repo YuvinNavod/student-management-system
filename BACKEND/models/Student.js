31213f9cdecc94f3a46587994374f0bf8621f988
YuvinNavod <yuvinnavod@gmail.com>
2024-06-07 13:16:56 +0530

    update

---
+++ BACKEND/models/Student.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema =mongoose.Schema;
+
+const StudentSchema =new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    age:{
+        type: Number,
+        required: true
+    },
+    gender:{
+        type: String,
+        required: true
+    }
+})
