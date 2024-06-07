f08673b83646ad4fb8e86556863c1f41bf6ce75b
YuvinNavod <137191227+YuvinNavod@users.noreply.github.com>
2024-06-08 00:33:49 +0530

    modify

--- BACKEND/routes/Students.js
+++ BACKEND/routes/Students.js
@@ -37,7 +37,7 @@ router.route("/update/:id").put(async(req,res)=>{
         gender
     }
     const update =await Student.findByIdAndUpdate(userId,updateStudent).then(()=>{
-        res.status(200).send({status:"User Updated",user:update});
+        res.status(200).send({status:"User Updated"});
     }).catch((err)=>{
         console.log(err);
         res.status(500).send({status:"Error With Updating Data",err:err.message});
@@ -55,13 +55,12 @@ router.route("/update/:id").put(async(req,res)=>{
         })
     })
 
-    router.route("/get/:id").get(async(req,res)=>{
+    router.route("/get/:id").get(async(req, res)=>{
         let userId= req.params.id;
-
-        const user= await Student.findById(userId).then(()=>{
-            res.status(200).send({user:user});
+        await Student.findById(userId).then((student)=>{
+            res.status(200).send({status:"User fetched",student});
         }).catch((err)=>{
-            console.log(err);
+            console.log(err.message);
             res.status(500).send({status:"can't find student"});
         })
 
