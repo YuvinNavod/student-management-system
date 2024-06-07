de867344b7670676728bab2d3ba6bfe17222ea79
YuvinNavod <137191227+YuvinNavod@users.noreply.github.com>
2024-06-07 14:20:34 +0530

    update

--- BACKEND/routes/Students.js
+++ BACKEND/routes/Students.js
@@ -1,4 +1,31 @@
 const router= require("express").Router();
 let Student = require("../models/Student");
 
-router.route("/add")
+router.route("/add").post((req,res)=>{
+    const name =req.body.name;
+    const age = Number(req.body.age);
+    const gender= req.body.gender;
+
+    const newStudent=new Student({
+        name,
+        age,
+        gender
+    })
+
+    newStudent.save().then(()=>{
+        res.json("Student Added");
+    }).catch((err)=>{
+        console.log(err);
+    })
+
+})
+
+router.route("/").get((req,res)=>{
+    Student.find().then((students)=>{
+        res.json(students);
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+module.exports= router;
